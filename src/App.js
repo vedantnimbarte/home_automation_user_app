e5e859e405b733bb68d44ff685700bb5332e35e9
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-23 14:10:41 +0530

    Removed stylesheet object and react native imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./navigation/navigation";
 
@@ -13,12 +12,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
