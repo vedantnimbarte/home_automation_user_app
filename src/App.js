1d39c3214553daab3a96676dc79dd55a3753f0a7
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-23 14:09:25 +0530

    Added navigation to app

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import Navigation from "./navigation/navigation";
 
 export default function App() {
   return (
     <NavigationContainer>
+      <Navigation />
       <StatusBar style="dark" />
     </NavigationContainer>
   );
