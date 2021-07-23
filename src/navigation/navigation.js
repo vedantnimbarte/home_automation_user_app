96186b66a3faba5b0e35d9f72753e5f8d8dc0535
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-23 17:37:21 +0530

    Added screen options and home screen to navigation

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,13 +1,30 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { SplashScreen } from "../screens";
+import { SplashScreen, Home } from "../screens";
+import { COLORS } from "../constants/theme";
 
 const Stack = createStackNavigator();
 
 export default function Navigation() {
   return (
-    <Stack.Navigator headerMode={false}>
+    <Stack.Navigator
+      screenOptions={{
+        headerTitle: "Capstone Automation",
+        headerTitleAlign: "center",
+        headerTintColor: COLORS.Background,
+        headerStyle: {
+          backgroundColor: COLORS.Primary,
+        },
+      }}
+    >
       <Stack.Screen name="SplashScreen" component={SplashScreen} />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerLeft: false,
+        }}
+      />
     </Stack.Navigator>
   );
 }
