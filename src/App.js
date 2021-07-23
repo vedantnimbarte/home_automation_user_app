b6206aff241a0234abef9260d29d42c81a7eabae
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-23 14:04:40 +0530

    Added native navigator

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="dark" />
-    </View>
+    </NavigationContainer>
   );
 }
 
