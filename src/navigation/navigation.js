42558034b7b89efd2ebff286d8a9d5761a5be090
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-26 19:01:56 +0530

    Added logout function to this page

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -2,10 +2,16 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { SplashScreen, Home, Room, Login } from "../screens";
 import { COLORS } from "../constants/theme";
+import { MaterialIcons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Stack = createStackNavigator();
 
 export default function Navigation() {
+  const _logout = async () => {
+    await AsyncStorage.removeItem("userDetails");
+  };
   return (
     <Stack.Navigator
       screenOptions={{
@@ -15,6 +21,16 @@ export default function Navigation() {
         headerStyle: {
           backgroundColor: COLORS.Primary,
         },
+        headerRight: () => (
+          <TouchableOpacity onPress={() => _logout()}>
+            <MaterialIcons
+              name="logout"
+              size={25}
+              color="red"
+              style={{ marginRight: 20 }}
+            />
+          </TouchableOpacity>
+        ),
       }}
     >
       <Stack.Screen
